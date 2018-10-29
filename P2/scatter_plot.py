2bde9e37c416a1eeea59fac4e61e15151d7b0adf
AnshKhurana <anshkhurana1507@gmail.com>
2018-10-30 00:02:30 +0530

    P2 by Ansh

---
+++ P2/scatter_plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+if __name__ == '__main__':
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    data = np.loadtxt('3dpd.out', delimiter=',')
+    x = data[:,0]
+    y = data[:,1]
+    z = data[:,2]
+    a = 1
+    b = -1
+    xr = data[np.where(a*data[:, 2] + b >= 0)]
+    ax.scatter(xr[:, 0], xr[:, 1], xr[:, 2], c='b', marker='o')
+    xb = data[np.where(a*data[:, 2] + b < 0)]
+    ax.scatter(xb[:, 0], xb[:, 1], xb[:, 2], c='r', marker='o')
+    plt.show()
